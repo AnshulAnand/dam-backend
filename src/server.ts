0c319f2ca207daf0576bc342fd70ce58dded82e1
AnshulAnand <anandanshul001@gmail.com>
2023-03-18 14:16:20 +0530

    feat: import & use routes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 import config from 'config'
 import express from 'express'
-import routes from './routes'
 import logger from './utils/logger'
 import connectDB from './utils/connect'
 
@@ -13,10 +12,21 @@ const app = express()
 app.use(express.json())
 app.use(deserializeUser)
 
+// importing routes
+import healthcheck from './routes/health-check.routes'
+import articles from './routes/articles.routes'
+import users from './routes/users.routes'
+import sessions from './routes/sessions.routes'
+
+// using routes
+app.use('/health-check', healthcheck)
+app.use('/articles', articles)
+app.use('/sessions', sessions)
+app.use('/users', users)
+
 const PORT = config.get<number>('port')
 
 app.listen(PORT, async () => {
   logger.info(`Server running on port ${PORT}`)
   await connectDB()
-  routes(app)
 })
