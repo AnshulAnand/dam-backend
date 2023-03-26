fcdc6fd0683f2659ad47f5cb8a7beb51c108fd81
AnshulAnand <anandanshul001@gmail.com>
2023-03-26 13:35:16 +0530

    feat: added routes

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,33 @@
-import dotenv from 'dotenv'
-dotenv.config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 import config from 'config'
 import logger from './utils/logger'
 import connectDB from './utils/connect'
+import errorHandler from './utils/errorHandler'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import corsOptions from './config/corsOptions'
 import express from 'express'
 
 connectDB() // connect to database
 
-
 const app = express()
 
+app.use(cors(corsOptions))
 app.use(express.json())
+app.use(cookieParser())
 
 // importing routes
 import healthcheck from './routes/health-check.routes'
+import userRoutes from './routes/user.routes'
+import articleRoutes from './routes/article.routes'
 
 // using routes
 app.use('/health-check', healthcheck)
+app.use('/users', userRoutes)
+app.use('/articles', articleRoutes)
 
 const PORT = config.get<number>('port')
 
-app.listen(PORT, () => logger.info(`Server running on port ${PORT}`) )
+app.use(errorHandler)
+
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`))
