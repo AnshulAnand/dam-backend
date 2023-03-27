6eaa4c96549181b95b3f8711d5bd7a6a1b3886a9
AnshulAnand <anandanshul001@gmail.com>
2023-03-27 15:01:30 +0530

    feat: /users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,15 +1,22 @@
 import express from 'express'
 import userController from '../controllers/user.controller'
-import validate from '../middleware/validateResource'
 import { createUserSchema } from '../schema/user.schema'
+import validate from '../middleware/validateResource'
 
 const router = express.Router()
 
+router.route('/users').get(userController.getAllUsers)
+
 router
-  .route('/')
-  .get(userController.getAllUsers)
-  .post(validate(createUserSchema), userController.createUser)
-  .patch(validate(createUserSchema), userController.updateUser)
-  .delete(validate(createUserSchema), userController.deleteUser)
+  .route('/register')
+  .post(validate(createUserSchema), userController.registerUser)
+
+router
+  .route('/login')
+  .post(validate(createUserSchema), userController.loginUser)
+
+router
+  .route('/logout')
+  .get(validate(createUserSchema), userController.logoutUser)
 
 export default router
